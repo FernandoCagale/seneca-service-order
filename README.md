351d5e017938020275bcdaa602db81c6fbce705f
José Fernando Bellucci Cagale <fernando.b.cagale@gmail.com>
2017-05-22 13:18:40 -0300

    init order

---
+++ README.md
@@ -0,0 +1,13 @@
+# seneca-service-order
+
+```sh
+$ npm install
+```
+
+`Starting MongoDB server`
+
+## Service
+
+```sh
+$ npm start
+```
---
+++ service.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const seneca = require('seneca')();
+const entity = require('seneca-entity');
+const mongoStore = require('seneca-mongo-store');
+const order = require('./lib/order');
+const dotenv = require('dotenv');
+
+dotenv.load({silent: true});
+
+const opts = {
+  mongo: {
+    uri: process.env.URI || 'mongodb://127.0.0.1:27017/seneca-order',
+    options: {}
+  }
+};
+
+seneca.use(entity);
+seneca.use(mongoStore, opts.mongo);
+seneca.use(order);
+seneca.listen({
+  pin: 'role:order,cmd:*',
+  port: process.env.PORT || 9001
+});
