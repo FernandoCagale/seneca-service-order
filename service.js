53e2fd3a8d8bd01fe0687682fa1c90de04c885da
FernandoCagale <fernando.b.cagale@gmail.com>
2017-06-29 20:25:38 -0300

    add mesh

--- service.js
+++ service.js
@@ -4,20 +4,45 @@ require('dotenv').load({silent: true});
 
 const seneca = require('seneca')();
 const entity = require('seneca-entity');
+const mesh = require('seneca-mesh');
+const consul = require('seneca-consul-registry');
 const mongoStore = require('seneca-mongo-store');
 const order = require('./lib/order');
+
+const PINS = [
+  'role:order,cmd:*',
+];
 
 const opts = {
   mongo: {
     uri: process.env.URI || 'mongodb://127.0.0.1:27017/seneca-order',
     options: {}
+  },
+  mesh: {
+    tag: 'base',
+    auto: true,
+    host: process.env.ADDR || '127.0.0.1',
+    listen: [{
+      pin: PINS,
+      host: process.env.ADDR || '127.0.0.1',
+      port: process.env.PORT || 50000 + Math.floor((10000 * Math.random()))
+    }],
+    discover: {
+      registry: {
+        active: true
+      },
+      multicast: {
+        active: false
+      }
+    }
+  },
+  consul: {
+    host: process.env.CONSUL_ADDR || '127.0.0.1'
   }
 };
 
+seneca.use(consul, opts.consul);
 seneca.use(entity);
 seneca.use(mongoStore, opts.mongo);
 seneca.use(order);
-seneca.listen({
-  pin: 'role:order,cmd:*',
-  port: process.env.PORT || 9001
-});
+seneca.use(mesh, opts.mesh);
