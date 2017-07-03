7ac6c395d8af544090063cf5075df65174894c7b
José Fernando Bellucci Cagale <fernando.b.cagale@gmail.com>
2017-07-03 12:35:22 -0300

    add test

---
+++ test/index.js
@@ -0,0 +1,151 @@
+const Lab = require('lab');
+const Code = require('code');
+const Seneca = require('seneca');
+const entity = require('seneca-entity');
+const mongoStore = require('seneca-mongo-store');
+
+const lab = exports.lab = Lab.script();
+const describe = lab.describe;
+const it = lab.it;
+const expect = Code.expect;
+const ROLE = 'order';
+
+const opts = {
+  mongo: {
+    uri: process.env.URI || 'mongodb://127.0.0.1:27017/seneca-order-test',
+    options: {}
+  }
+};
+
+function testSeneca (fin) {
+  return Seneca({log: 'test'})
+    .use(entity)
+    .use(mongoStore, opts.mongo)
+    .test(fin)
+    .use(require('../lib/order'));
+}
+
+describe('test ORDER', () => {
+  let _id;
+
+  it('create', (fin) => {
+    const seneca = testSeneca(fin);
+
+    const pattern = {
+      role: ROLE,
+      cmd: 'create',
+      emission: '01/06/2017',
+      price: 25.00,
+      client: 'Client test'
+    };
+
+    seneca
+    .gate()
+    .act(pattern, (ignore, result) => {
+      expect(result.order.emission).to.equal('01/06/2017');
+      expect(result.order.price).to.equal(25.00);
+      expect(result.order.client).to.equal('Client test');
+      expect(result.order.id).to.exist();
+      expect(result.ok).to.equal(true);
+      _id = result.order.id;
+    })
+    .ready(fin);
+  });
+
+  it('findAll', (fin) => {
+    const seneca = testSeneca(fin);
+
+    const pattern = {
+      role: ROLE,
+      cmd: 'findAll'
+    };
+
+    seneca
+    .gate()
+    .act(pattern, (ignore, result) => {
+      expect(result.orders).to.exist();
+      expect(result.ok).to.equal(true);
+    })
+    .ready(fin);
+  });
+
+  it('findById', (fin) => {
+    const seneca = testSeneca(fin);
+
+    const pattern = {
+      role: ROLE,
+      cmd: 'findById',
+      id: _id
+    };
+
+    seneca
+    .gate()
+    .act(pattern, (ignore, result) => {
+      expect(result.order.emission).to.equal('01/06/2017');
+      expect(result.order.price).to.equal(25.00);
+      expect(result.order.client).to.equal('Client test');
+      expect(result.order.id).to.exist();
+      expect(result.ok).to.equal(true);
+    })
+    .ready(fin);
+  });
+
+  it('update', (fin) => {
+    const seneca = testSeneca(fin);
+
+    const pattern = {
+      role: ROLE,
+      cmd: 'create',
+      id: _id,
+      emission: '06/06/2017',
+      price: 50.00,
+      client: 'Client test - alter'
+    };
+
+    seneca
+    .gate()
+    .act(pattern, (ignore, result) => {
+      expect(result.order.emission).to.equal('06/06/2017');
+      expect(result.order.price).to.equal(50.00);
+      expect(result.order.client).to.equal('Client test - alter');
+      expect(result.order.id).to.exist();
+      expect(result.ok).to.equal(true);
+    })
+    .ready(fin);
+  });
+
+  it('remove', (fin) => {
+    const seneca = testSeneca(fin);
+
+    const pattern = {
+      role: ROLE,
+      cmd: 'remove',
+      id: _id
+    };
+
+    seneca
+    .gate()
+    .act(pattern, (ignore, result) => {
+      expect(result.ok).to.equal(true);
+    })
+    .ready(fin);
+  });
+
+  it('findById not found', (fin) => {
+    const seneca = testSeneca(fin);
+
+    const pattern = {
+      role: ROLE,
+      cmd: 'findById',
+      id: _id
+    };
+
+    seneca
+    .gate()
+    .act(pattern, (ignore, result) => {
+      expect(result.ok).to.equal(false);
+      expect(result.why).to.equal('ID not found');
+    })
+    .ready(fin);
+  });
+});
